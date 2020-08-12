70e9c59761caa775204c9ad12df847a0543006a2
Gavin Grant <gavingrant@gmail.com>
2020-08-12 13:01:13 -0700

    revised morganSetting value

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const POKEDEX = require('./pokedex.json')
 
 const app = express()
 
-const morganSetting = process.env.NODE_ENV === 'production'
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
